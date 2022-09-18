4df9b8297510a2af9ce17e0e8da1c1422120d938
pavelV <psvodnev@gmail.com>
2022-09-18 21:36:58 +0300

    end

--- Task_Fi/Program.cs
+++ Task_Fi/Program.cs
@@ -7,3 +7,29 @@
 //     либо задать на старте выполнения алгоритма.
 //       При решении не рекомендуется пользоваться коллекциями,
 //      лучше обойтись исключительно массивами.
+
+Console.Write("Введите элементы массива через пробел: ");
+string[] initArray = Console.ReadLine().Split(new char[] { ' ' });
+
+int i2 = 0;
+for (int i = 0; i < initArray.Length; i++)
+{
+    if (initArray[i].Length <= 3) i2++;
+}
+string[] resultArray = new string[i2];
+
+i2 = 0;
+for (int i = 0; i < initArray.Length; i++)
+{
+    if (initArray[i].Length <= 3)
+    {
+        resultArray[i2] = initArray[i];
+        i2++;
+    }
+}
+Console.Write("Массив, содержащий до 3х символов = [ ");
+for (i2 = 0; i2 < resultArray.Length; i2++)
+{
+    Console.Write($"'{resultArray[i2]}' ");
+}
+Console.Write("]");
